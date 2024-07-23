1666eadaf4b1b96254abe9e4497ebbc9accd1e37
Victor Chin <116740251+Chinjila@users.noreply.github.com>
2024-07-23 13:02:30 -0700

    Comments on the codes and format

--- LearnSpectreUI/Program.cs
+++ LearnSpectreUI/Program.cs
@@ -5,12 +5,15 @@ namespace LearnSpectreUI
 {
     internal class Program
     {
-       static string rock = "✊";
+        //declare these emoji at class level so all static method can access them
+        //if declared in Main, other methods such as GetComputerHand and CompareHands won't see them
+        //becaus stack
+        static string rock = "✊";
         static string scissor = "✌";
         static string paper = "✋";
         static void Main(string[] args)
         {
-            Console.OutputEncoding = Encoding.UTF8;
+            Console.OutputEncoding = Encoding.UTF8;//turns on emoji support
             do
             {
                 var myHand = AnsiConsole.Prompt(
@@ -20,32 +23,38 @@ namespace LearnSpectreUI
                  .AddChoices(new[] {
                     rock,paper,scissor
                  }));
-
+                //Use a seperate static method to generate a random computer hand, reduce Main clutter
                 string computerHand = GetComputerHand();
 
-               string whoWins = CompareHand(myHand, computerHand);
+                //Use a seperate static method to compare hands, reduce Main clutter
+                string whoWins = CompareHand(myHand, computerHand);
+
                 // Echo the hand back to the terminal
                 AnsiConsole.WriteLine($"You hand: {myHand} vs {computerHand} ");
-                
+
+                // Show who wins with Markup
                 AnsiConsole.MarkupInterpolated($"[bold yellow on blue]{whoWins}[/]\n");
-                //AnsiConsole.WriteLine($"{whoWins}");
-            } while (AnsiConsole.Confirm("Pick Again?"));
+
+            } while (AnsiConsole.Confirm("Pick Again?")); //loop to start another game with Confirm Prompt
         }
 
         private static string CompareHand(string myHand, string computerHand)
         {
+            //logic to determin winner, tie condition
             if (myHand == computerHand) { return "Its a tie."; }
+            //win conditions
             if (myHand == rock && computerHand == scissor) { return "You win"; }
             if (myHand == scissor && computerHand == paper) { return "You win"; }
             if (myHand == paper && computerHand == rock) { return "You win"; }
+            //if none of the three conditions is true, then the final return is computer wins
             return "Computer wins";
         }
 
         private static string GetComputerHand()
         {
             string[] hands = { rock, paper, scissor };
-            Random rnd = new Random();
-            return hands[rnd.Next(hands.Length)];
+            Random rnd = new Random();//create a new instance to generate random number
+            return hands[rnd.Next(hands.Length)]; // pick a random hand
         }
     }
 }
