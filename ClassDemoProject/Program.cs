d2c8818743cb0142ed375aa2cf98423794ffbaea
Victor Chin <116740251+Chinjila@users.noreply.github.com>
2024-07-23 11:19:46 -0700

    Add project files.

---
+++ ClassDemoProject/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using VictorDemo;
+
+
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car("Victor",FuelType.Hybrid,4);
+            Car bobCar = new Car("Bob", FuelType.Diesel, 3);
+            myCar.Drive(15085);
+            bobCar.Drive(3234);
+            Console.WriteLine($"{myCar.VIN} has {myCar.OdometerInMiles} miles.");
+            Console.WriteLine($"{bobCar.VIN} has {bobCar.OdometerInKM} KM.");
+
+            //System.Environment
+            Console.WriteLine($"Program is running from {System.Environment.CurrentDirectory}");
+            Console.WriteLine($"Your username is {System.Environment.UserName}");
+            Console.WriteLine($"Computer has {System.Environment.ProcessorCount} processors");
+            Console.WriteLine($"Your document folder is {System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)}" );
+            Console.WriteLine($"Your PC is named:{System.Environment.MachineName}");
+
+            //Print out ALL Environment Variables
+            //System.Environment.GetEnvironmentVariables()
+
+            Console.WriteLine($"{new String('=', 40)}");
+            foreach (DictionaryEntry dictionaryEntry in System.Environment.GetEnvironmentVariables())
+            {   
+                Console.WriteLine($"{dictionaryEntry.Key,20}={dictionaryEntry.Value,-20}");
+            }
+
+            Console.WriteLine($"{new String('=', 40)}");
+
+            Random rnd = Random.Shared; // single instance that shared across method and threads.
+            Random rnd2 = new Random(); //every unique instance has a different seed
+            string[] rps = ["rock", "paper", "scissor"];
+
+            for (int i = 0; i < 50; i++) {
+                Console.WriteLine($"computer throws : {rps[rnd.Next(3)]}");
+            }
+            
+        }
+    }
+}
+
---
+++ ClassDemoProject/Samples.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VictorDemo
+{
+    //these are all types that exists under namespace
+    internal class Car{
+        //Properties - are method so field can be read(get) or set
+        internal FuelType TypeOfFuel { 
+            get { return _fuelType; }
+        }
+        internal int PassengerCapacity => _passengerCount;
+        internal string VIN => _vin;
+        internal decimal OdometerInMiles => Math.Round(_odometer / 1600M,1);
+        internal decimal OdometerInKM => Math.Round(_odometer / 1000,1);
+        //Field -- is the actual data,
+        // fields are usually private so its only accessible by other members of the class
+        private int _passengerCount; //4 bytes
+        private FuelType _fuelType; //4byte
+        private string _vin; //4bytes
+        private decimal _odometer = 0;//16bytes
+
+        //Methods - functionality(method),Constructor,Indexer,Finalizer,Deconstructor 
+        //This is a constructor where the method has NO return, and has the same name as the class
+        internal Car(string vin,FuelType ft,int passengers) {
+            _vin = vin;
+            _fuelType = ft;
+            _passengerCount= passengers;
+        }
+        //This is a regular method called Drive
+        internal void Drive(decimal distanceInMeters) {
+            _odometer += distanceInMeters;
+        }
+
+    }
+    internal record Record1 { }
+    internal struct SampleStruct { }
+    internal interface IDemo;
+    internal enum FuelType
+    {
+        Gas=0,
+        Hybrid=1,
+        Electric=2,
+        E85=3,
+        Diesel=4
+    }
+    internal enum CardSuit { 
+        Spade,
+        Heart,
+        Club,
+        Diamond
+    }
+    internal delegate int MyDelegate(int x, int y);
+
+}
